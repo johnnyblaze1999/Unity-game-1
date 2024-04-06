7316e3e9ccbf817263649457d0e572b823d0d262
Vinh B <vinh.bui0101@gmail.com>
2024-04-06 15:53:36 -0700

    updated

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -1,9 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private AudioMixer myMixer;
-    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private Text volumeTextUI = null;
+
+    private void Start()
+    {
+        LoadValues();
+    }
+    public void volumeSlider(float volume)
+    {
+        volumeTextUI.text = volume.ToString("0.0");
+
+    }
+
+    public SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues(){
+        float volumeValue = PlayerPrefs.GetFloat("volumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
 }
