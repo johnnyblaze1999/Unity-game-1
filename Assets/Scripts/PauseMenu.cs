f71b8940705a21cbb4981c298542de6d3805eb47
VinnyBui <vinh.bui0101@gmail.com>
2024-04-29 18:11:10 -0700

    Fixed ExitGame btn

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -28,6 +28,10 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void ExitGame(){
-        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
